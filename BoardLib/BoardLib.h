635c8625d90f9a3e18b13d9ec54621136f2d7660
ali.afzal <ali.afzal@nxb.com.pk>
2023-02-03 15:20:39 +0500

    Initial Commit

---
+++ BoardLib/BoardLib.h
@@ -0,0 +1,18 @@
+//
+//  BoardLib.h
+//  BoardLib
+//
+//  Created by Ali Afzal on 03/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoardLib.
+FOUNDATION_EXPORT double BoardLibVersionNumber;
+
+//! Project version string for BoardLib.
+FOUNDATION_EXPORT const unsigned char BoardLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoardLib/PublicHeader.h>
+
+
